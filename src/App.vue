d6ea3ae1dc5fd580da56ca4bc218d7c2ebfb9e1a
Leo <coder.leo.le@gmail.com>
2019-05-23 15:13:38 +0700

    Add fake navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
-  <b-container id="app">
-    <div class="taiger-content-container">
-      <router-view/>
+  <div class="vue-container">
+    <div class="navbar-container">
+      <NavBar />
     </div>
-  </b-container>
+    <b-container class="taiger-content-container">
+      <router-view/>
+    </b-container>
+  </div>
 </template>
 
+<script>
+import NavBar from '@/components/common/NavBar/index.vue'
+
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
+
 <style lang="scss" scoped>
-#app {
+.taiger-content-container {
   padding-top: 40px;
-  padding-bottom: 20px;
+  padding-bottom: 30px;
 }
 /* #nav {
   padding: 30px;
---
+++ src/components/common/NavBar/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="vue-container">
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-brand href="#">iConverse</b-navbar-brand>
+      <b-navbar-nav>
+        <b-nav-item href="#" active>Reporting</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
